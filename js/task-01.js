48c358de95d3191f7a41cbfe6dce11cd3dc34a29
Olga-alexsandrova <berduginaolga5@gmail.com>
2023-10-02 14:19:14 +0300

    task 1-3

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+
+
+ const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('li.item');
+ console.log(`Number of categories: ${categoryItems.length}`);
+
+categoryItems.forEach((categoryItem) => {
+ const categoryTitle = categoryItem.querySelector('h2').textContent;
+ const categoryElements = categoryItem.querySelectorAll('ul li');
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElements.length}`);
+});
+
+
+
+
+
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ulIngredients = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  ulIngredients.appendChild(li);
+});
